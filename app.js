d6786ee6c2519983729cd7e437f5506cf4a1fd0c
Mia Wong <miaw629@gmail.com>
2020-06-17 17:01:37 -0400

    add path to connect to client

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: ".env.development" });
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const { db, port } = require("./config");
 
 //
@@ -26,9 +27,9 @@ mongoose
     .catch((err) => console.log(err));
 app.get("/", (req, res) => res.send("App connected"));
 app.use("/api", require("./routes/routes"));
-
+app.use(express.static("client/build"));
 app.get("*", (req, res) => {
-    res.send("app connected! default undefined routes");
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 app.listen(port, (err) => {
     console.log(`Server live on port: ${port}`);
