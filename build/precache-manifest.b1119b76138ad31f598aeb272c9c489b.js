586f68b138a58272a377a62cd45c06e9e1f0ffb9
Mia Wong <miaw629@gmail.com>
2020-06-19 12:14:29 -0400

    move build

---
+++ build/precache-manifest.b1119b76138ad31f598aeb272c9c489b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af48877e82de71d5d2691779691d62c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cec961d1b0b0de1ff1b",
+    "url": "/static/css/main.7e615ff4.chunk.css"
+  },
+  {
+    "revision": "7375e95f796dd1351c86",
+    "url": "/static/js/2.7538e0ca.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "/static/js/2.7538e0ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cec961d1b0b0de1ff1b",
+    "url": "/static/js/main.b91d2ef2.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "7f27f338f794c41e20aa9355a98cec83",
+    "url": "/static/media/Creamy-Salmon-Pasta-with-Spinach-2.7f27f338.jpg"
+  },
+  {
+    "revision": "b8c3f81d0745771d0686b80f7ba887fc",
+    "url": "/static/media/blankimage.b8c3f81d.jpg"
+  },
+  {
+    "revision": "d59aa53e675a0f794d488c8c3a18834a",
+    "url": "/static/media/cover.d59aa53e.png"
+  },
+  {
+    "revision": "9b135b6b8ce4c7fb1fe4422302448d4d",
+    "url": "/static/media/munchies.9b135b6b.png"
+  },
+  {
+    "revision": "cdad9470849d9825666d8e2e2d1eedff",
+    "url": "/static/media/ramen.cdad9470.png"
+  }
+]);
