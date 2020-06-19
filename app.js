f52521961771849417f63434f6696a22db7edbc2
Mia Wong <miaw629@gmail.com>
2020-06-19 17:23:50 -0400

    change cors url

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.use(express.json());
 
 // allow CORS
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header(
+        "Access-Control-Allow-Origin",
+        "http://cookin-env-1.eba-cvmkzrir.us-east-1.elasticbeanstalk.com/"
+    ); // update to match the domain you will make the request from
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
