386a63d5df5bfcbfc60926e75fec1b8689a03c4f
Mia Wong <miaw629@gmail.com>
2020-06-19 18:28:24 -0400

    add cors package

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const { db, port } = require("./config");
+const cors = require("cors");
 
 //
 const app = express();
 // a method inbuilt in express to recognize the incoming request object as a JSON object.
 app.use(express.json());
+app.use(cors());
 
 // allow CORS
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+//     next();
+// });
 
 mongoose
     .connect(db, { useNewUrlParser: true })
