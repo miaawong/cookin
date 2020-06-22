c4398399c8d9aa942ab2828f708a403ae25d0251
Mia Wong <miaw629@gmail.com>
2020-06-22 14:28:56 -0400

    remove getallrecipes

--- client/src/pages/recipe.js
+++ client/src/pages/recipe.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import RecipeComponent from "../recipes/components/recipe/Recipe";
 import { useParams, useHistory } from "react-router-dom";
 import { connect, useDispatch } from "react-redux";
-import { getCurrentRecipe, getAllRecipes } from "../recipes/recipeAction";
+import { getCurrentRecipe, getAllUserRecipes } from "../recipes/recipeAction";
 import * as recipeActionTypes from "../recipes/recipeActionTypes";
 import EditRecipe from "./EditRecipe";
 
@@ -11,8 +11,7 @@ const Recipe = ({ currentRecipe, JWToken, edit, userId, loggedIn }) => {
     const history = useHistory();
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(getCurrentRecipe(recipeId, JWToken, history));
-        dispatch(getAllRecipes(JWToken));
+        dispatch(getCurrentRecipe(recipeId, history));
         return () => {
             dispatch({
                 type: recipeActionTypes.CLEAR_CURRENT_RECIPE,
@@ -22,7 +21,6 @@ const Recipe = ({ currentRecipe, JWToken, edit, userId, loggedIn }) => {
     }, []);
 
     if (edit) {
-        console.log("edit is true");
         return <EditRecipe />;
     } else {
         return (
