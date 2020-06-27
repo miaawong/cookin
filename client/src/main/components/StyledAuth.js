696b421542dd8f2ea0770c4815c5f2ce5d8d5622
Mia Wong <miaw629@gmail.com>
2020-06-26 22:46:20 -0400

    remove image for ipad

--- client/src/main/components/StyledAuth.js
+++ client/src/main/components/StyledAuth.js
@@ -12,35 +12,24 @@ export const StyledMain = styled(Main)`
 export const ImageContainer = styled.div`
     height: 30%;
     width: 50%;
-    @media ${device.small} {
+    margin: 0 auto;
+    @media ${device.small}, ${device.medium}, ${device.large} {
         display: none;
     }
-    @media ${device.medium}, ${device.large} {
-        width: 100%;
-    }
-    margin: 0 auto;
 `;
 
 export const Img = styled.img`
     width: 75%;
     object-fit: contain;
-    @media ${device.small}, ${device.medium}, ${device.large} {
-        width: 100%;
-        height: 100%;
-    }
 `;
 
 export const Title = styled.h1`
     @media ${device.small} {
         font-size: ${(props) => props.theme.fontSizes.large};
-        /* margin-bottom: 0;
-        padding-bottom: 0; */
     }
 `;
 export const Check = styled.h3`
     @media ${device.small} {
         font-size: ${(props) => props.theme.fontSizes.small};
-        margin-top: 0;
-        padding-top: 0;
     }
 `;
